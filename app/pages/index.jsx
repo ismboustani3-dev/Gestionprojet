b95978405c41478895fb404d59a9ef3a63d42919
ismboustani3-dev <ism.boustani3@gmail.com>
2026-05-05 14:05:40 +0100

    Update index.jsx

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,1 +1,12 @@
+import projects from "../data/projects.json";
+import ProjectCard from "../components/ProjectCard";
 
+export default function Home() {
+  return (
+    <div className="grid grid-cols-3 gap-4 p-6">
+      {projects.map((p, i) => (
+        <ProjectCard key={i} project={p} />
+      ))}
+    </div>
+  );
+}
